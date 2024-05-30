25a8ca8082eecee90cff2c6cdb652ec1e8840c9d
AkisKourouklis <kourouklis@proton.me>
2024-05-30 12:17:37 +0300

    added alytics and page speed metrics

--- packages/blog-starter-kit/themes/hashnode/pages/_app.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/_app.tsx
@@ -1,22 +1,26 @@
-import { AppProps } from 'next/app';
-import { withUrqlClient } from 'next-urql';
+import {AppProps} from 'next/app';
+import {withUrqlClient} from 'next-urql';
 import 'tailwindcss/tailwind.css'
+import {SpeedInsights} from "@vercel/speed-insights/next"
+import {Analytics} from "@vercel/analytics/react"
 
 import '../styles/index.css';
-import { GlobalFontVariables } from '../components/fonts';
-import { getUrqlClientConfig } from '../lib/api/client';
+import {GlobalFontVariables} from '../components/fonts';
+import {getUrqlClientConfig} from '../lib/api/client';
 
-import { Fragment } from 'react';
+import {Fragment} from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<Fragment>
-			 <GlobalFontVariables />
-			 <Component {...pageProps} />
-		</Fragment>
-	);
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <Fragment>
+      <SpeedInsights/>
+      <Analytics/>
+      <GlobalFontVariables/>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
 // `withUrqlClient` HOC provides the `urqlClient` prop and takes care of restoring cache from urqlState
 // this will provide ssr cache to the provider and enable to use `useQuery` hook on the client side
-export default withUrqlClient(getUrqlClientConfig, { neverSuspend: true })(MyApp);
+export default withUrqlClient(getUrqlClientConfig, {neverSuspend: true})(MyApp);
